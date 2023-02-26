d2493f29226d0d0be945961a5a49ea0981521bfa
unknown <rectyexploit@gmail.com>
2023-02-27 02:17:11 +0800

    First commit

--- src/PenjualanTiket.java
+++ src/PenjualanTiket.java
@@ -6,18 +6,28 @@ public class PenjualanTiket extends javax.swing.JFrame {
     private ResultSet rs;
     private DefaultTableModel model = null;
     koneksi k = new koneksi();
+   
     /**
      * Creates new form PenjualanTiket
      */
     public PenjualanTiket() {
         initComponents();
         k.connect();
-        refreshTable();
+        this.refreshTable();
+        this.setEnableComponent();
+    }
+    
+    public void setEnableComponent(){
+         this.inputHargaTiket.setEnabled(false);
+         this.inputDiskon.setEnabled(false);
+         this.inputSubTotal.setEnabled(false);
+         this.inputTotalBayar.setEnabled(false);
     }
 
     class tiket extends PenjualanTiket{
         int id;
-        String nama,tujuan,member,JumlahTiket,subtotal,diskon,totalbayar,hargaTiket;
+        boolean selected;
+        String nama,tujuan,member,JumlahTiket,diskon,subtotal,totalbayar,hargaTiket;
         public tiket() {
             this.id = 0;
             this.tujuan = inputTujuan.getSelectedItem().toString();
@@ -107,6 +117,7 @@ public class PenjualanTiket extends javax.swing.JFrame {
         btnDelete = new javax.swing.JButton();
         btnUpdate = new javax.swing.JButton();
         inputId = new javax.swing.JTextField();
+        btnHitung = new javax.swing.JButton();
 
         jTextField2.setText("jTextField2");
 
@@ -140,8 +151,47 @@ public class PenjualanTiket extends javax.swing.JFrame {
         jLabel9.setText("Diskon");
 
         inputTujuan.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "--Pilih Tujuan--", "Jakarta", "Bali", "Surabaya" }));
+        inputTujuan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputTujuanActionPerformed(evt);
+            }
+        });
+
+        inputJumlahTiket.addInputMethodListener(new java.awt.event.InputMethodListener() {
+            public void inputMethodTextChanged(java.awt.event.InputMethodEvent evt) {
+                inputJumlahTiketInputMethodTextChanged(evt);
+            }
+            public void caretPositionChanged(java.awt.event.InputMethodEvent evt) {
+            }
+        });
+        inputJumlahTiket.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputJumlahTiketActionPerformed(evt);
+            }
+        });
+        inputJumlahTiket.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                inputJumlahTiketKeyPressed(evt);
+            }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                inputJumlahTiketKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                inputJumlahTiketKeyTyped(evt);
+            }
+        });
 
         checkMember.setText("Ya");
+        checkMember.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                checkMemberStateChanged(evt);
+            }
+        });
+        checkMember.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                checkMemberMouseClicked(evt);
+            }
+        });
         checkMember.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 checkMemberActionPerformed(evt);
@@ -190,6 +240,13 @@ public class PenjualanTiket extends javax.swing.JFrame {
             }
         });
 
+        btnHitung.setText("HITUNG");
+        btnHitung.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHitungActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -218,8 +275,9 @@ public class PenjualanTiket extends javax.swing.JFrame {
                             .addComponent(jLabel9)
                             .addComponent(jLabel7)
                             .addComponent(jLabel8)
-                            .addComponent(jLabel5))
-                        .addGap(70, 70, 70)
+                            .addComponent(jLabel5)
+                            .addComponent(btnHitung, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(inputTotalBayar, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(checkMember, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -245,62 +303,78 @@ public class PenjualanTiket extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(inputId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 308, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(checkMember, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5))
-                        .addGap(44, 44, 44)
-                        .addComponent(jLabel6))
-                    .addComponent(inputHargaTiket, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(inputDiskon, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9))
-                .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(inputSubTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel7))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(inputTotalBayar, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8))
-                .addGap(159, 159, 159))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jLabel1)
-                .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(inputNama, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel10))
-                    .addComponent(jLabel2))
-                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(inputId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 308, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(inputTujuan, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(checkMember, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel5))
+                                .addGap(44, 44, 44)
+                                .addComponent(jLabel6))
+                            .addComponent(inputHargaTiket, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(inputJumlahTiket, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4)))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(21, 21, 21)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnRekam, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(inputDiskon, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel9))
+                        .addGap(23, 23, 23)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(inputSubTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel7))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(inputTotalBayar, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel8))
+                        .addGap(33, 33, 33))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel1)
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(inputNama, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel10))
+                            .addComponent(jLabel2))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(inputTujuan, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel3))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(inputJumlahTiket, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel4)))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(21, 21, 21)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRekam, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(btnHitung, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(73, 73, 73))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void checkMemberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkMemberActionPerformed
+
+        tiket t = new tiket();
+        t.selected = this.checkMember.isSelected();
+        int hargaT = Integer.parseInt(t.hargaTiket);
+        
+        if (t.selected) {
+             t.diskon = String.valueOf(0.1 * hargaT);
+             this.inputDiskon.setText(t.diskon);
+        } else {
+             t.diskon = String.valueOf(0);
+        }
+        
         // TODO add your handling code here:
     }//GEN-LAST:event_checkMemberActionPerformed
 
@@ -316,7 +390,7 @@ public class PenjualanTiket extends javax.swing.JFrame {
             stat.setString(5, t.member);
             stat.setString(6, t.hargaTiket);
             stat.setString(7, t.subtotal);
-            stat.setString(8, t.diskon);
+            stat.setString(8, String.valueOf(t.diskon));
             stat.setString(9, inputTotalBayar.getText());
             stat.executeUpdate();
             refreshTable();
@@ -365,7 +439,7 @@ public class PenjualanTiket extends javax.swing.JFrame {
             stat.setString(4, t.member);
             stat.setString(5, t.hargaTiket);
             stat.setString(6, t.subtotal);
-            stat.setString(7, t.diskon);
+            stat.setString(7, String.valueOf(t.diskon));
             stat.setString(8, inputTotalBayar.getText());
             stat.setString(9,inputId.getText());
             stat.executeUpdate();
@@ -376,6 +450,78 @@ public class PenjualanTiket extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }//GEN-LAST:event_btnUpdateActionPerformed
+
+    private void checkMemberStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_checkMemberStateChanged
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_checkMemberStateChanged
+
+    private void checkMemberMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_checkMemberMouseClicked
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_checkMemberMouseClicked
+
+    private void inputJumlahTiketActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputJumlahTiketActionPerformed
+           
+    }//GEN-LAST:event_inputJumlahTiketActionPerformed
+
+    private void inputTujuanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputTujuanActionPerformed
+        tiket t = new tiket();
+        if(null == t.tujuan){
+            t.hargaTiket = String.valueOf(0);
+            this.inputHargaTiket.setText(String.valueOf(0));
+        }else switch (t.tujuan) {
+            case "Jakarta":
+                t.hargaTiket = String.valueOf(950000);
+                this.inputHargaTiket.setText(String.valueOf(950000));
+                break;
+        // TODO add your handling code here:
+            case "Bali":
+                 t.hargaTiket = String.valueOf(1200000);
+                this.inputHargaTiket.setText(String.valueOf(1200000));
+                break;
+            default:
+                t.hargaTiket = String.valueOf(1000000);
+                this.inputHargaTiket.setText(String.valueOf(1000000));
+                break;
+        }
+    }//GEN-LAST:event_inputTujuanActionPerformed
+
+    private void inputJumlahTiketInputMethodTextChanged(java.awt.event.InputMethodEvent evt) {//GEN-FIRST:event_inputJumlahTiketInputMethodTextChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputJumlahTiketInputMethodTextChanged
+
+    private void inputJumlahTiketKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_inputJumlahTiketKeyPressed
+         
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputJumlahTiketKeyPressed
+
+    private void inputJumlahTiketKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_inputJumlahTiketKeyTyped
+        
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputJumlahTiketKeyTyped
+
+    private void inputJumlahTiketKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_inputJumlahTiketKeyReleased
+         tiket t = new tiket();
+        int hargaT = Integer.parseInt(t.hargaTiket);
+        int jmlT = Integer.parseInt(t.JumlahTiket);
+        int hasil = hargaT * jmlT; 
+        t.subtotal = String.valueOf(hasil);
+        this.inputSubTotal.setText(String.valueOf(hasil)); 
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputJumlahTiketKeyReleased
+
+    private void btnHitungActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHitungActionPerformed
+        tiket t = new tiket();
+        int subT = Integer.parseInt(t.subtotal);
+        double diskon = Double.parseDouble(t.diskon);
+        double hasil = subT - diskon;
+        t.totalbayar = String.valueOf(hasil);
+        this.inputTotalBayar.setText(String.valueOf(hasil));
+        
+
+// TODO add your handling code here:
+    }//GEN-LAST:event_btnHitungActionPerformed
 
     /**
      * @param args the command line arguments
@@ -414,6 +560,7 @@ public class PenjualanTiket extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnHitung;
     private javax.swing.JButton btnRekam;
     private javax.swing.JButton btnUpdate;
     private javax.swing.JCheckBox checkMember;
