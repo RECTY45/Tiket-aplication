175216ccaebc1117eb1bd7d39a55a01d9a8a5924
unknown <rectyexploit@gmail.com>
2023-02-26 23:31:17 +0800

    First commit

---
+++ src/koneksi.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+ private String url="jdbc:mysql://localhost/uh_202048";
+ private String username_laragon = "root";
+ private String password_laragon = "";
+ private Connection con;
+ 
+ 
+ public void connect(){
+     try {
+         con = DriverManager.getConnection(url, username_laragon, password_laragon);
+         System.out.println("Databases Connect Success!");
+     } catch (Exception e) {
+         JOptionPane.showMessageDialog(null, e.getMessage());
+     }
+ 
+ }
+
+    public Connection getCon() {
+        return con;
+    }
+    
+}
